fac91eb68dbb80b962335302d40d1f341b85cf60
wangjun861205 <444055828@qq.com>
2019-06-23 09:28:23 +0800

    为Expr类型添加Field插值解析

--- expr.go
+++ expr.go
@@ -1,12 +1,75 @@
 package nborm
 
-type Expr string
+import (
+	"fmt"
+	"io"
+	"strings"
+)
 
-func NewExpr(sqlexpr string) *Expr {
-	expr := Expr(sqlexpr)
-	return &expr
+type expStat int
+
+const (
+	normal  expStat = 0
+	inQuote expStat = 1
+)
+
+type Expr struct {
+	exp    string
+	fields []Field
+}
+
+func NewExpr(sqlexpr string, fields ...Field) *Expr {
+	return &Expr{
+		sqlexpr,
+		fields,
+	}
 }
 
 func (e *Expr) String() string {
-	return string(*e)
+	if len(e.fields) == 0 {
+		return e.exp
+	}
+	reader := strings.NewReader(e.exp)
+	stat := normal
+	quoteStack := make([]rune, 0, 8)
+	var builder strings.Builder
+	for r, _, err := reader.ReadRune(); ; r, _, err = reader.ReadRune() {
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			panic(err)
+		}
+		if r == '"' || r == '`' || r == '\'' {
+			if stat == normal {
+				stat = inQuote
+			} else {
+				if quoteStack[len(quoteStack)-1] == r {
+					quoteStack = quoteStack[:len(quoteStack)-1]
+					if len(quoteStack) == 0 {
+						stat = normal
+					}
+				} else {
+					quoteStack = append(quoteStack, r)
+				}
+			}
+		}
+		if r == '?' {
+			if stat == normal {
+				builder.WriteString(e.fields[0].fullColName())
+				e.fields = e.fields[1:]
+			} else {
+				builder.WriteRune(r)
+			}
+		} else {
+			builder.WriteRune(r)
+		}
+	}
+	if stat != normal {
+		panic(fmt.Errorf("no closed expression (exp: %s)", e.exp))
+	}
+	if len(e.fields) != 0 {
+		panic(fmt.Errorf("extra field for express (fields: %v)", e.fields))
+	}
+	return builder.String()
 }
